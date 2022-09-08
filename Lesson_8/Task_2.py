be5b6113f2d41315dba4b608dcada476c540e79e
tonykava <112110058+tonykava@users.noreply.github.com>
2022-09-08 19:07:27 +0300

    Update Task_2.py

--- Lesson_8/Task_2.py
+++ Lesson_8/Task_2.py
@@ -2,9 +2,7 @@ def make_country(country, capital):
     dic = {country: capital}
     return dic
 
-a = "Ukraine"
-b = 'Kyiv'
-print(make_country(a, b))
+print(make_country('Ukraine', 'Kyiv'))
 
 
 
