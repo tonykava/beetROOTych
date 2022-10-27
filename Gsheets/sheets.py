56821aa844e8849fdf1ad2d6a0b0949539529ebc
tonykava <112110058+tonykava@users.noreply.github.com>
2022-10-27 21:03:09 +0300

    Update sheets.py

--- Gsheets/sheets.py
+++ Gsheets/sheets.py
@@ -3,18 +3,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
 creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-
 client = gspread.authorize(creds)
-
 sheet = client.open('Plakhta').sheet1
 
 
 loaded_hours = {}
+
 names = [''.join(i) for i in sheet.get('B3:B242')]
-
-
 logoped = [''.join(i) for i in sheet.get('M3:M242')]
 
 for i in range(len(logoped)):
@@ -27,7 +23,6 @@ sample = ['9', '10', '11', '12', '14', '15', '16']
 for i in loaded_hours:
     free_hours[i] = [j for j in sample if j not in loaded_hours[i]]
 
-pprint(free_hours)
 
 hours_priority = {'9': 0,
                   '10': 0,
@@ -42,6 +37,7 @@ for i in hours_priority:
         if i in free_hours[j]:
             hours_priority[i] += 1
 
+
 def sort_hours_priority(time):
     d = {}
     s = sorted(time.items(), key=lambda x: x[1])
@@ -52,15 +48,22 @@ def sort_hours_priority(time):
 
 final_dic = {}
 
-for i in free_hours:
-    if list(hours_priority)[0] in free_hours[i]:
-        final_dic[i] = list(hours_priority)[0]
-        free_hours[i].remove(list(hours_priority)[0])
-        hours_priority.pop(list(hours_priority)[0])
-        for j in free_hours[i]:
-            hours_priority[j] -= 1
-        hours_priority = sort_hours_priority(hours_priority)
 
-print(final_dic)
+while hours_priority != {}:
+    for i in free_hours:
+        if list(hours_priority)[0] in free_hours[i]:
+            final_dic[i] = list(hours_priority)[0]
+            for j in free_hours[i]:
+                if j in hours_priority:
+                    hours_priority[j] -= 1
+            hours_priority.pop(list(hours_priority)[0])
+            hours_priority = sort_hours_priority(hours_priority)
+    for i in final_dic:
+        if i in free_hours:
+            free_hours.pop(i)
 
+for i in final_dic:
+    final_dic[i] += ':00'
+
+pprint(final_dic)
 
